cb55937be54a001c0fdabfaab1cb001860dc2856
TylerKnox65 <tylerj652@deltalearns.ca>
2023-10-26 13:04:35 -0700

    Done

--- task01.py
+++ task01.py
@@ -73,7 +73,7 @@ class main():
     def dataEntry(self): #Enter selection mode
         while True:
             try:
-                choice = int(input(("Welcome to assignment helper!\nWith this program you can (1) Create assigment, (2) Edit assignment scores, (3) Delete data, (4) View current assignments:  ")))
+                choice = int(input(("\nWelcome to assignment helper!\nWith this program you can (1) Create assigment, (2) Edit assignment scores, (3) Delete data, (4) View current assignments, (5) Quit Program:  ")))
                 break
             except:
                 print("Enter a valid integer.")
@@ -92,23 +92,79 @@ class main():
             except:
                 self.dataEntry()
                     
-            self.delete()
-    def writeData(self): #Write Data to file
+            #self.delete()
+        elif choice  == 4:
+            self.getData()
+        elif choice == 5:
+            os._exit(0)
+        elif choice == 8264:
+            self.EldenOstasiewicz()
+        self.dataEntry()
+    def writeData(self): #Write Data to file, unused
         pass
     def editData(self): #Edit current data
-        pass
+        
+        with open('data.csv', 'r') as file:
+            data = file.read()
+            prev = json.loads(data)
+            while True:
+                possiblenames = [i for i in prev if i != '0']
+                name = str(input(f"Enter the assignment name, possible names are {possiblenames}: "))
+                try:
+                    possibleStudents = [i for i in prev[name]]
+                    StudentName = str(input(f"Enter the Student that you wish to change the mark of's name, student's avaliable are {possibleStudents}: "))
+                    LaurenRoller = input(f"This student has a mark of {prev[name][StudentName]}, what do you want to change it to?: ")
+                    break
+                except:
+                    print("Enter a valid name")
+        with open('data.csv',"r+") as file:
+            
+            #LaurenRoller = input(f"This student has a mark of {prev[name][StudentName]}, what do you want to change it to?: ")
+            prev[name][StudentName] = LaurenRoller
+            file.write(json.dumps(prev))
     def getData(self): #Finds the data
-        pass
+       with open('data.csv',"r") as file:
+            data = json.loads(file.read())
+            for i in data:
+                if i == '0':
+                    pass
+                else:
+                    print(f"\nFor the assignment {i}:")
+                    for o in data[i]:
+                        print(f'Student {o} has a mark of {data[i][o]}')
+    def EldenOstasiewicz(self):
+        for i in range(100):
+            print("Secret easter egg?!?!? How did you find it:?:")
+            os.startfile("H:\Documents\Fax\Inbox\WelcomeFax.tif")
     def createAssignment(self):
         name = str(input("Enter the assignment name: "))
-        value = str(input("Enter the assignment value: "))
-        with open('data.csv') as file:
-            pass
+        #value = str(input("Enter the assignment value: "))
+        while True:
+            try:
+                numStudent = int(input("Enter the amount of students that have a mark: "))
+                break
+            except:
+                print("Enter a valid amount of students")
+        with open('data.csv', 'r') as file:
+            data = file.read()
+        with open('data.csv',"r+") as file:
+            prev = json.loads(data)
+            prev[f"{name}"] = {}
+            print(prev)
+            for i in range(numStudent):
+                studentName = str(input("Enter the student's name: "))
+                studentMark = str(input(f"Enter the student: {studentName}'s mark: "))
+                prev[f"{name}"][f"{studentName}"] = f"{studentMark}"
+            file.write(json.dumps(prev))
+            print("Saved!")
         
+
+        #you are doing some coding
     def delete(self): #THIS DELETES ALL DATA SAVED
        os.remove("data.csv")
        with open("data.csv", "w") as file:
-           file.write("{0:0}")
+           file.write('{"0":8264}')
+           
 
 
 
